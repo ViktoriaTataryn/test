c527ce612c0e94bad4482b826aab64eb7d2af087
ViktoriaTataryn <viktoriatatarin252@gmail.com>
2025-02-17 21:13:13 +0200

    HelloEndpoint

--- Test/Test/Controllers/TestController.cs
+++ Test/Test/Controllers/TestController.cs
@@ -5,9 +5,16 @@ namespace Test.Controllers;
 [Route("api/test")]
 public class TestController:ControllerBase
 {
+    //new
+    [HttpGet]
+    public ActionResult SayHello2()
+    {
+        return Ok("Hello");
+    }
     [HttpGet]
     public ActionResult SayHello()
     {
         return Ok("Hello");
     }
+    
 }
--- Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b82530bc1c0f9523f6b9b80163faeb8effffc20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
